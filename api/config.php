a521af7826e3223830a27e0eea9897f010c71edd
riumat <matteoariu42@gmail.com>
2024-04-24 22:50:43 +0200

    edit README

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database
+{
+  public $db_name = "ristorantedb";
+  public $host = "localhost";
+
+  //credenziali mysql
+  public $user = "root";
+  public $password = "password";
+
+}
+
+
+
+
+
